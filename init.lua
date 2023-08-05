1ce44fb019a9d250442ceb0a5a302dfc332ad377
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2023-08-06 02:09:59 +0700

    Adding global lualine and making it more readable

--- init.lua
+++ init.lua
@@ -5,8 +5,9 @@ require('plugins')
 require('options')
 require('keymaps')
 
-vim.cmd[[colorscheme onedark]]
+vim.cmd('colorscheme onedark')
 
 vim.cmd("autocmd ColorScheme * highlight Pmenu guibg=NONE")
+vim.cmd('autocmd VimEnter * set laststatus=3')
 
 vim.o.termguicolors = true
