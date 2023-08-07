f0045396a9e610f6e3306cf9e9825b375071f8f3
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2023-08-07 13:19:50 +0700

    Revert "Adding global lualine and making it more readable"
    This reverts commit 1ce44fb019a9d250442ceb0a5a302dfc332ad377.

--- init.lua
+++ init.lua
@@ -5,9 +5,8 @@ require('plugins')
 require('options')
 require('keymaps')
 
-vim.cmd('colorscheme onedark')
+vim.cmd[[colorscheme onedark]]
 
 vim.cmd("autocmd ColorScheme * highlight Pmenu guibg=NONE")
-vim.cmd('autocmd VimEnter * set laststatus=3')
 
 vim.o.termguicolors = true
--- lua/config/ui/lualine.lua
+++ lua/config/ui/lualine.lua
@@ -52,6 +52,20 @@ lualine.setup {
     },
     lualine_x = {
       {
+        noice.api.status.message.get_hl,
+        cond = noice.api.status.message.has,
+      },
+      {
+        noice.api.status.command.get,
+        cond = noice.api.status.command.has,
+        color = { fg = "#ff9e64" },
+      },
+      {
+        noice.api.status.mode.get,
+        cond = noice.api.status.mode.has,
+        color = { fg = "#ff9e64" },
+      },
+      {
         noice.api.status.search.get,
         cond = noice.api.status.search.has,
         color = { fg = "#ff9e64" },
